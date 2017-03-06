98e882bbef9615f6ba555ab682eacc863f8cfaf8
Rsotosan <sotosanchezraul@gmail.com>
2017-03-06 12:35:44 +0100

    Changes

--- Dispute/src/main/java/com/dispute/Application.java
+++ Dispute/src/main/java/com/dispute/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DisputeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DisputeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
