d32f0146ec2131ae9cb7642e26d9b4557a315ee7
Rsotosan <sotosanchezraul@gmail.com>
2017-03-17 03:13:27 +0100

    Fixes

--- src/main/java/com/dispute/security/SecurityConfiguration.java
+++ src/main/java/com/dispute/security/SecurityConfiguration.java
@@ -2,10 +2,12 @@ package com.dispute.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -24,14 +26,13 @@ protected void configure(HttpSecurity http) throws Exception {
 		// Private pages (all other pages)
         http.authorizeRequests().antMatchers("/newTeam*").hasAnyRole("USER");
         http.authorizeRequests().antMatchers("/newTournament*").hasAnyRole("USER");
-        
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/tournament/*").hasAnyRole("USER");
         // Login form
         http.formLogin().loginPage("/login");
         http.formLogin().usernameParameter("username");
         http.formLogin().passwordParameter("password");
         http.formLogin().defaultSuccessUrl("/");
         http.formLogin().failureUrl("/login?loginError=true");
-        
         // Logout
         http.logout().logoutUrl("/logout");
         http.logout().logoutSuccessUrl("/");
