feb170fedc9143f30039d344ce8712f7f70dd9bb
Rsotosan <sotosanchezraul@gmail.com>
2017-03-21 01:50:38 +0100

    fixes

--- src/main/java/com/dispute/tournament/MatchUpRepository.java
+++ src/main/java/com/dispute/tournament/MatchUpRepository.java
@@ -3,4 +3,5 @@ package com.dispute.tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MatchUpRepository extends JpaRepository<MatchUp, Long>{
+	MatchUp findById(Long id);
 }
