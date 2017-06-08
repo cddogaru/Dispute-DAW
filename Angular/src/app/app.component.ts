a8c1c6af6c403281e18022cace98133dc4922572
dimi3sinculotes <rufus111296@gmail.com>
2017-06-09 01:05:24 +0200

    mmmmm
    mmmmm

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './header.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ 'app.component.css']
+  styleUrls: ['../assets/styles/bootstrap.css', '../assets/styles/style.css', '../assets/styles/bootstrap.min.css' ]
 })
 export class AppComponent {
 }
