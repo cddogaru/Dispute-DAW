11079728ad7156b20f1b8f644390a41546f22088
Raúl Soto <sotosanchezraul@gmail.com>
2017-06-22 20:33:29 +0200

    Boostrap

--- Angular/src/app/header.component.ts
+++ Angular/src/app/header.component.ts
@@ -1,9 +1,8 @@
 ﻿import { Component } from '@angular/core';
 
 @Component({
-    selector: 'as-header-component',
-    templateUrl: './header.html',
-    styleUrls: ['../assets/styles/style.css']
+    selector: 'header',
+    templateUrl: './header.component.html',
 })
 
 export class HeaderComponent { }
